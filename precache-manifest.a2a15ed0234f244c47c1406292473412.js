b8f2171b40e20c539c20e4eb177fac9aa9582fea
Rafix111 <rafaldoranczyk@gmail.com>
2019-02-04 22:02:01 +0100

    Updates

---
+++ precache-manifest.a2a15ed0234f244c47c1406292473412.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "f880cf94ef251dbec505",
+    "url": "/MemoryGame/static/js/runtime~main.f880cf94.js"
+  },
+  {
+    "revision": "c993ebd99fa6243feccd",
+    "url": "/MemoryGame/static/js/main.c993ebd9.chunk.js"
+  },
+  {
+    "revision": "a1a45af2c32ab017cee6",
+    "url": "/MemoryGame/static/js/1.a1a45af2.chunk.js"
+  },
+  {
+    "revision": "b7ad70b5741dde53587030654e3b2365",
+    "url": "/MemoryGame/index.html"
+  }
+];
