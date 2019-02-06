19fc96072ed043aae686ac2bf9ccd054a5869c87
Rafix111 <rafaldoranczyk@gmail.com>
2019-02-06 23:30:35 +0100

    Updates

--- precache-manifest.30d1453dc64af1a08766c94e32ab720d.js
+++ precache-manifest.30d1453dc64af1a08766c94e32ab720d.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/MemoryGame/static/js/runtime~main.f880cf94.js"
   },
   {
-    "revision": "7e956b19980f41552fd2",
-    "url": "/MemoryGame/static/js/main.7e956b19.chunk.js"
+    "revision": "6ba77d8f918bb8d51440",
+    "url": "/MemoryGame/static/js/main.6ba77d8f.chunk.js"
   },
   {
     "revision": "3a054fcefe41e9ccbe73",
     "url": "/MemoryGame/static/js/1.3a054fce.chunk.js"
   },
   {
-    "revision": "deb2c51a23aab7dd587224cabe48c41c",
+    "revision": "d4a03e993823697529653c067cdd39e0",
     "url": "/MemoryGame/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/MemoryGame/precache-manifest.ef697a3282072521e9de747d653b59c7.js"
+  "/MemoryGame/precache-manifest.30d1453dc64af1a08766c94e32ab720d.js"
 );
 
 workbox.clientsClaim();
